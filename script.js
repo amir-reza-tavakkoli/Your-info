091c449960d1f026164498a1d7d56bb944649139
FOLI <you@example.com>
2021-09-27 19:11:23 +0330

    add basic data for finding IP

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-let getDateOrTime = (date) => (date == 'date') ? new Date().toLocaleDateString() : new Date().toLocaleTimeString();
+let getDateOrTime = (date) => (date == 'date') ? new Date().toLocaleDateString()  : (date == "time") ? new Date().toLocaleTimeString() : 'Invalid';
+
 let nightOrDay = (postfix) => postfix[-2] == 'A' ? 'Day' : 'night';
 
+let getIP = async () => {
+    // let url = 'https://geo.ipify.org/api/v1?apiKey=at_0jKVsS1q5y340ZINLeu6lEc4ws8di';
+let response = await fetch(url);
+console.log("im in")
+let object = await response.json();
+return object;
+}
+
+let getAdvice = (t) => t == 'night' ? 'dont saty out too long tho! 🙄' : 'have a nice day 🤗';
 
 (function general() {
-
-    let date = document.querySelector(".general-data > div:last-child p:first-child em")
-    date.innerText = getDateOrTime('date');
-    let time = document.querySelector(".general-data > div:last-child p:nth-child(2) em")
-    let time2 = time.innerText = getDateOrTime('notdate')
-    document.querySelector(".general-data > div:last-child p:nth-child(3) em").innerText = nightOrDay(time2)
-
-
-
+    let general = document.querySelector(".general-data > div:last-child");
+    let day = general.children[0].firstElementChild
+    day.innerText = getDateOrTime('date');
+    let time = general.children[1].firstElementChild
+    let timeInDay = time.innerText = getDateOrTime('time');
+    let t = nightOrDay(timeInDay);
+    general.children[2].firstElementChild.innerText = t;
+    general.children[3].innerText = getAdvice(t);
 }());
 
+(function geolocation() {
+    let data = getIP();
+    console.log(data.ip);
+    let geolocation = document.querySelector(".geolocation > div:last-child");
+    geolocation.children[0].firstElementChild.innerText = 'ttt'
+}())
+// let url = 'https://geo.ipify.org/api/v1?apiKey=at_0jKVsS1q5y340ZINLeu6lEc4ws8di';
+// let response = await fetch(url);
 
+// let commits = await response.json();
+// console.log(commits.ip)
