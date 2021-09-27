dea201e0e9e6446addeb4399e2638a0582c0f0d4
FOLI <you@example.com>
2021-09-27 19:42:58 +0330

    add vpn status

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ let getDateOrTime = (date) => (date == 'date') ? new Date().toLocaleDateString()
 let nightOrDay = (postfix) => postfix[-2] == 'A' ? 'Day' : 'night';
 
 let getIP = async () => {
-    // let url = 'https://geo.ipify.org/api/v1?apiKey=at_0jKVsS1q5y340ZINLeu6lEc4ws8di';
-let response = await fetch(url);
-console.log("im in")
-let object = await response.json();
-return object;
+    //  let url = 'https://geo.ipify.org/api/v1?apiKey=at_0jKVsS1q5y340ZINLeu6lEc4ws8di';
+    let response = await fetch(url);
+
+    let geoObject = await response.json();
+    return geoObject;
+}
+
+function changeGeo(element, geoObject) {
+    element.children[0].firstElementChild.innerText = geoObject.ip;
+    element.children[2].firstElementChild.innerText = geoObject.location.region;
+    element.children[3].firstElementChild.innerText = geoObject.location.country
+    element.children[4].firstElementChild.innerText = geoObject.location.city
+    handleVPN(geoObject);
+}
+
+let handleGeo = async (elem) => {
+let geoObject = await getIP();
+changeGeo(elem, geoObject);
 }
 
 let getAdvice = (t) => t == 'night' ? 'dont saty out too long tho! 🙄' : 'have a nice day 🤗';
@@ -24,13 +37,15 @@ let getAdvice = (t) => t == 'night' ? 'dont saty out too long tho! 🙄' : 'have
 }());
 
 (function geolocation() {
-    let data = getIP();
-    console.log(data.ip);
-    let geolocation = document.querySelector(".geolocation > div:last-child");
-    geolocation.children[0].firstElementChild.innerText = 'ttt'
+    // console.log(data.ip);
+    let geoElement = document.querySelector(".geolocation > div:last-child");
+    let data = handleGeo(geoElement);
+    // geolocation.children[0].firstElementChild.innerText = 'ttt'
 }())
-// let url = 'https://geo.ipify.org/api/v1?apiKey=at_0jKVsS1q5y340ZINLeu6lEc4ws8di';
-// let response = await fetch(url);
 
-// let commits = await response.json();
-// console.log(commits.ip)
+function handleVPN(geoObject) {
+    let handleVPN = document.querySelector(".vpn > div:last-child");
+    handleVPN.children[0].firstElementChild.innerText = geoObject.proxy.proxy;
+    handleVPN.children[1].firstElementChild.innerText = geoObject.proxy.vpn;
+
+}
