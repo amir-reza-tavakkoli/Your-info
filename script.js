d0dd5961560be7b0663e288729fc78b0d49f1f9f
FOLI <you@example.com>
2021-09-27 20:39:46 +0330

    organize files

--- script.js
+++ script.js
@@ -1,51 +1,48 @@
-let getDateOrTime = (date) => (date == 'date') ? new Date().toLocaleDateString()  : (date == "time") ? new Date().toLocaleTimeString() : 'Invalid';
+let getDateOrTime = (date) => (date == 'date') ? new Date().toLocaleDateString() : (date == "time") ? new Date().toLocaleTimeString() : 'Invalid';
 
-let nightOrDay = (postfix) => postfix[-2] == 'A' ? 'Day' : 'night';
+let getRelativeTime = (timePostfix) => timePostfix[-2] == 'A' ? 'Day' : 'night';
 
-let getIP = async () => {
+let getAdvice = (dayorNight) => dayorNight == 'night' ? 'dont saty out too long tho! 🙄' : 'have a nice day 🤗';
+
+let getIPObject = async () => {
     //  let url = 'https://geo.ipify.org/api/v1?apiKey=at_0jKVsS1q5y340ZINLeu6lEc4ws8di';
     let response = await fetch(url);
-
     let geoObject = await response.json();
     return geoObject;
 }
 
-function changeGeo(element, geoObject) {
+function showGeo(element, geoObject) {
     element.children[0].firstElementChild.innerText = geoObject.ip;
     element.children[2].firstElementChild.innerText = geoObject.location.region;
-    element.children[3].firstElementChild.innerText = geoObject.location.country
-    element.children[4].firstElementChild.innerText = geoObject.location.city
-    handleVPN(geoObject);
+    element.children[3].firstElementChild.innerText = geoObject.location.country;
+    element.children[4].firstElementChild.innerText = geoObject.location.city;
+    vpn(geoObject);
 }
 
 let handleGeo = async (elem) => {
-let geoObject = await getIP();
-changeGeo(elem, geoObject);
+    let geoObject = await getIPObject();
+    showGeo(elem, geoObject);
 }
 
-let getAdvice = (t) => t == 'night' ? 'dont saty out too long tho! 🙄' : 'have a nice day 🤗';
-
+// each card specific IIF
 (function general() {
-    let general = document.querySelector(".general-data > div:last-child");
-    let day = general.children[0].firstElementChild
-    day.innerText = getDateOrTime('date');
-    let time = general.children[1].firstElementChild
+    let generalElement = document.querySelector(".general > div:last-child");
+    generalElement.children[0].firstElementChild.innerText = getDateOrTime('date');
+    let time = generalElement.children[1].firstElementChild;
     let timeInDay = time.innerText = getDateOrTime('time');
-    let t = nightOrDay(timeInDay);
-    general.children[2].firstElementChild.innerText = t;
-    general.children[3].innerText = getAdvice(t);
+    let relativeTime = getRelativeTime(timeInDay);
+    generalElement.children[2].firstElementChild.innerText = relativeTime;
+    generalElement.children[3].innerText = getAdvice(relativeTime);
 }());
 
 (function geolocation() {
-    // console.log(data.ip);
     let geoElement = document.querySelector(".geolocation > div:last-child");
-    let data = handleGeo(geoElement);
-    // geolocation.children[0].firstElementChild.innerText = 'ttt'
+    handleGeo(geoElement);
 }())
 
-function handleVPN(geoObject) {
-    let handleVPN = document.querySelector(".vpn > div:last-child");
-    handleVPN.children[0].firstElementChild.innerText = geoObject.proxy.proxy;
-    handleVPN.children[1].firstElementChild.innerText = geoObject.proxy.vpn;
-
+// gets invoked after fetching geoObject
+function vpn(geoObject) {
+    let vpnElement = document.querySelector(".vpn > div:last-child");
+    vpnElement.children[0].firstElementChild.innerText = geoObject.proxy.proxy;
+    vpnElement.children[1].firstElementChild.innerText = geoObject.proxy.vpn;
 }
